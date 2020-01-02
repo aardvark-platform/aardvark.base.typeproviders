c354a97df734bfb090bbd359d3773b696ecd4f62
Georg Haaser <krauthaufen@awx.at>
2020-01-02 13:27:04 +0100

    added FSharp.Core to package

--- build.fsx
+++ build.fsx
@@ -6,15 +6,30 @@ open System
 open System.IO
 open System.Diagnostics
 open Aardvark.Fake
+open Fake.Core
+open Fake.Core.Operators
+open Fake.Core.TargetOperators
 
 do Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
 
 DefaultSetup.install ["src/Aardvark.Base.TypeProviders.sln"]
 
 
+Target.create "CopyFSharpCore" (fun _ ->
+    let core =
+        if config.debug then Path.Combine("bin", "Debug", "net45", "FSharp.Core.dll")
+        else Path.Combine("bin", "Release", "net45", "FSharp.Core.dll")
+
+    File.Copy(core, Path.Combine("pack", "typeproviders", "fsharp41", "net45", "FSharp.Core.dll"), true)
+)
+
+
 #if DEBUG
 do System.Diagnostics.Debugger.Launch() |> ignore
 #endif
+
+
+"Compile" ==> "CopyFSharpCore" ==> "CreatePackage"
 
 
 entry()
